8ef8d084a760f6c4a8e3c10952e2f93bb63e9ced
JiyongLiu <814329735@qq.com>
2013-12-31 18:51:22 +0800

    add dictionary file parser program

---
+++ parseDictionary.py
@@ -0,0 +1,18 @@
+import os
+import json
+import string
+
+class dictionary(object):
+    def myNo(self):
+        self.domains=0;
+        pass
+
+    def __init__(self):
+        pass
+
+    def search(self, word):
+    	print word+" search"
+    	pass
+
+
+
